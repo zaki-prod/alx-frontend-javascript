51990ff764d7f66e24ba34ec6f59b9792fff1b9d
zaki-prod <zakiprod@gmail.com>
2024-10-02 08:58:19 +0000

    10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
+  }
+
+  return 
